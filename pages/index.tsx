53cf029cbb40a4530b6b796e76a7a56a6c0e35ec
sonicDev99 <clevercode99@gmail.com>
2022-10-24 14:04:32 +0530

    add hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 const Home: NextPage = () => {
     return (
@@ -11,6 +12,10 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header />
+
+            <main className="bg-[#E7ECEE]">
+                <Hero />
+            </main>
         </div>
     );
 };
